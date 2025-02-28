3221aa7e59afd7e2a638baaac1f028b7bea43a73
sunbi-s <78129508+sunbi-s@users.noreply.github.com>
2025-02-28 14:21:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ chat = ChatOpenAI(
 
 # 프롬프트 설정
 chat_prompt = ChatPromptTemplate.from_messages([
-    ("system", "아주 짧게 말해."),
+    ("system", "너는 재료역학 전문가야 재료역학에 대해서 설명해줘."),
     ("user", "{user_input}"),
 ])
 
